5c3a55a632cd54d94c66095ac60f6bdcff64855c
fahmi2307201 <113483525+fahmi2307201@users.noreply.github.com>
2023-05-07 17:50:57 +0700

    Add files via upload

---
+++ Pertemuan5 SetterDanGetter/SetterDanGetter.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Perpustakaan {
+protected:
+  string buku = "Sejarah Indonesia";
+  string nama = "Fahmi";
+
+public:
+  string cekPinjaman() {
+    return "Buku Sejarah Indonesia Sudah Dipinjam Oleh Fahmi";
+  }
+};
+
+class Pengembalian : public Perpustakaan {
+protected:
+  string pengembalian = "Telah di kembalikan";
+
+public:
+  string cekKembali() {
+    return "Buku Sejarah Indonesia Sudah Dikembalikan oleh Fahmi ";
+  }
+};
+
+
+int main() {
+  Pengembalian bukuPerpustakaan;
+
+  cout << bukuPerpustakaan.cekPinjaman() << endl;
+  cout << bukuPerpustakaan.cekKembali() << endl;
+
+  return 0;
+}
